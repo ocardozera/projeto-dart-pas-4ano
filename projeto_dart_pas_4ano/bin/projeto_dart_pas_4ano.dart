0de0d1f5f9ea98cace6f86ced1f1018dc9b63647
Matheus Cardoso <matheusjosecardoso@outlook.com>
2022-04-27 21:26:44 -0300

    Commit inicial

---
+++ projeto_dart_pas_4ano/bin/projeto_dart_pas_4ano.dart
@@ -0,0 +1,5 @@
+import 'package:projeto_dart_pas_4ano/projeto_dart_pas_4ano.dart' as projeto_dart_pas_4ano;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projeto_dart_pas_4ano.calculate()}!');
+}
---
+++ projeto_dart_pas_4ano/test/projeto_dart_pas_4ano_test.dart
@@ -0,0 +1,8 @@
+import 'package:projeto_dart_pas_4ano/projeto_dart_pas_4ano.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
